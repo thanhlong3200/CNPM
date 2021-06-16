262cf73a1eb6a1b66a65dcdb69de8b5d5fa6ba4c
thanhlong3200 <18130131@st.hcmuaf.edu.vn>
2021-06-16 21:56:56 +0700

    testheroku

--- src/main/java/com/fashi/constants/DBConfiguration.java
+++ src/main/java/com/fashi/constants/DBConfiguration.java
@@ -3,11 +3,11 @@ package com.fashi.constants;
 public class DBConfiguration {
 	public class DbConfiguration {
 		 
-	    public static final String HOST_NAME = "localhost";
-	    public static final String DB_NAME = "fashi";
+	    public static final String HOST_NAME = "remotemysql.com";
+	    public static final String DB_NAME = "GAY4DNCVEB";
 	    public static final String DB_PORT = "3306";
-	    public static final String USER_NAME = "root";
-	    public static final String PASSWORD = "016961193922";
+	    public static final String USER_NAME = "GAY4DNCVEB";
+	    public static final String PASSWORD = "QBGsvaSscl";
 	    public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
 	    public static final int DB_MIN_CONNECTIONS = 2;
 	    public static final int DB_MAX_CONNECTIONS = 4;
